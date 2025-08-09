f29da739c5a42b9abcfa6e6ca164f866831bdd87
hannycodes <hananseidmohammedh@gmail>
2025-08-09 21:37:37 +0300

    First week task

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App(){
+  return(
+    <div className="home">
+      <Header>
+        <h1>Welcome To My Islamic To-Do App</h1>
+      </Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header(){
+    return(
+        <header className="header">
+            <nav className="nav-bar">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Tasks</a></li>
+                    <li><a href="">Explore</a></li>
+                    <li><a href="">About</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
